3e4e2d221e53014e8cbc4f72e4f4cff090c5d5d1
Tim Broughton <TimBroughton@quickenloans.com>
2019-01-02 13:38:25 -0500

    created app scaffolding

---
+++ BoredWebApp/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BoredWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult One()
+        {
+            return View();
+        }
+
+        public ActionResult Specific()
+        {
+
+            return View();
+        }
+
+        public ActionResult Multiple()
+        {
+
+            return View();
+        }
+    }
+}
---
+++ BoredWebApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BoredWebApp.MvcApplication" Language="C#" %>
